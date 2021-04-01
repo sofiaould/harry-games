f594b4c1ec52b6fd9dfa93a7b1ec4b17e5d2de49
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-04-01 20:39:16 +0200

    audio left

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 /** @format */
-
 let harry;
-let mechants = [];
+let mechants;
 let gameover = false;
 let points;
 const ctx = document.querySelector("canvas").getContext("2d");
 const W = ctx.canvas.width;
 const H = ctx.canvas.height;
-var endOfTheGame = document.querySelector("gameOver");
-console.log(endOfTheGame);
+let endOfTheGame = document.querySelector("#gameOver");
+//let buttonStart = document.querySelector("button");
 /*var myAudio = document.createElement("audio");
-myAudio.src = "HarryPottermusic.mp3";
+myAudio.src = "./audio/HarryPottermusic.mp3";
 myAudio.play();*/
-
+var audio = document.querySelector("#audio");
 function draw() {
   ctx.clearRect(0, 0, W, H);
   //
@@ -31,30 +30,25 @@ function draw() {
     el.y += 1;
     el.draw();
   });
-
   for (var i = 0; i < mechants.length; i++) {
     mechants[i].x -= 6;
   }
-
   //
   for (vilain of mechants) {
     if (harry.getPoints(vilain)) {
       console.log(points);
     }
   }
-
   //Points
   ctx.font = "16px arial";
   ctx.fillStyle = "#DFAF2C";
   ctx.fillText(`Score: ${points}`, W - 130, 50);
 }
-
 document.onkeydown = function (e) {
   if (!harry) return;
   switch (e.keyCode) {
     case 38: // up arrow
       harry.moveUp();
-
       break;
     case 40: // down arrow
       harry.moveDown();
@@ -71,28 +65,32 @@ let raf;
 let frames = 0;
 function animLoop() {
   // executee toutes les 16ms
-
   frames++;
-
   draw();
   checkGameOver();
-
   if (!gameover) {
     requestAnimationFrame(animLoop);
-  } /* else {
-    restartGame();
-  }*/
+  } else {
+    endOfTheGame.style.display = "block";
+    document.getElementById("start-button").onclick = function () {
+      ctx.clearRect(0, 0, W, H);
+      startGame();
+    };
+  }
 }
 function startGame() {
-  // endOfTheGame.style.display = "none";
-
+  endOfTheGame.style.display = "none";
   if (raf) {
     cancelAnimationFrame(raf);
   }
   //
+  mechants = [];
+  gameover = false;
   harry = new Component(50, 100, "./images/harry.png", 20, 200);
   //obstacles = [];
   points = 0;
+  audio.volume = 0.2;
+  audio.play();
   requestAnimationFrame(animLoop);
   //console.log("reussi");
 }
@@ -101,31 +99,11 @@ function checkGameOver() {
   const crashed = mechants.some(function (vilain) {
     return harry.crashWith(vilain);
   });
-
   if (crashed) {
     gameover = true;
-    //endOfTheGame.style.display = "block";
-    //let endOfTheGame = setTimeout(startGame(), 1000);
-    //clearTimeout(endOfTheGame);
-    //stopGame();
-    /*document.createElement("canvas").getContext("2d");
-    context.font = "16px arial";
-    context.fillStyle = "#FFFFF";
-    context.fillText(`Game Over`, W - 130, 50);*/
+    // document.getElementById("start").style.display = "none";
   }
 }
-/*function restartGame() {
-  let harry = 0;
-  frames = 0;
-  let mechants = [];
-  let gameover = false;
-  startGame();
-}*/
-/*function stopGame() {
-  //return clearInterval(this.interval);
-  //console.log("you made it");
-  let endOfTheGame = setTimeout(startGame(), 1000);
-  if (crashed) {
-    clearTimeout(endOfTheGame);
-  }
-}*/
+//document.getElementById("start").onclick = function () {
+//startGame();
+//};
